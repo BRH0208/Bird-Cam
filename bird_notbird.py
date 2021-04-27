f7660c1340159e472bc716b7fcbca6ab99e989d1
BRH0208 <bryce.hawken@s.stemk12.org>
2021-04-27 16:31:04 -0600

    Added Binary Identification
    When the model is given 2 datasets, instead of creating a softmax it creates a binary sigmoid. This is only partial, requiring more testing but it is a good starting point that allows for binary training.

--- bird_notbird.py
+++ bird_notbird.py
@@ -1,84 +1,58 @@
 #Imports
 from PIL import Image as PIL
+import pandas as pd
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten, BatchNormalization
+from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras.layers import Input, Conv1D, Conv2D, MaxPooling1D, MaxPooling2D, Dense, Dropout, Activation, Flatten, BatchNormalization
+from tensorflow.keras.utils import to_categorical
 import cv2 as cv
 from os import path
 from os import listdir
-from os.path import join
+from os.path import isfile, join
 import random
 from math import ceil
 
+
 #Much of this code was taken from https://medium.com/@mrgarg.rajat/training-on-large-datasets-that-dont-fit-in-memory-in-keras-60a974785d71
 
-# ConfigVars
+##ConfigVars
 datasetName = "Dataset"
 saveLocation = "SavedModels\\Checkpoints\\cp-{epoch:04d}.ckpt"
 exportLocation = "SavedModels\\ExportedModels\\model_export"
-saveLoad = True				# Look for a recent save. If it exists: Load it before training
+saveLoad = True #Look for a recent save. If it exists: Load it before training
 exportWhenComplete = False
 randomSeed = 12345
 batchSize = 128
-imgSize = (224,224) 		# 32 v 32 reccomended
-numEpochs = 0 				# 0 for no training(Usefull for exporting)
+imgSize = (32,32) ##32 v 32 reccomended
+numEpochs = 50 #0 for no training(Usefull for exporting)
 sharedVerbose = 1
-isGrayScale = False 
-splitPercentage = 0.1
+isGrayScale = True 
+splitPercentage = 0.05
 internalLayers = [64,64]
-dropoutAmount = 0.5 		# Used to prevent overfitting. 0.25 is more than plenty, 0 to disable
+dropoutAmount = 0.35 #Used to prevent overfitting. 0.25 is more than plenty, 0 to disable
 camNum = 0
 
+##Session Management
+#config = tf.compat.v1.ConfigProto()
+#config.gpu_options.per_process_gpu_memory_fraction = 0.9
+#session = tf.compat.v1.Session(config=config)
 
-# Functions & Classes
-class KerasGeneratorFromFile(keras.utils.Sequence):
-    def __init__(self, fileArray, batch_size) :
-        self.fileArray = fileArray
-        self.batch_size = batch_size
-
-    def __len__(self):
-        return int(ceil(float(len(self.fileArray)) / float(self.batch_size)))
-
-    def __getitem__(self,idx):
-        #assert (((idx+1) * self.batch_size) <= len(self.fileArray))
-        data = []
-        labels = []
-        for fileID in range((idx * self.batch_size),((idx+1) * self.batch_size)-1):
-            if(fileID >= len(self.fileArray)):
-                break
-            data.append(prepareFileImage(self.fileArray[fileID][0]))
-            labels.append(self.fileArray[fileID][1]) ##This is run every time a batch is requested, it is run a bunch and it isn't the fastest. Too Bad!
-        if(isGrayScale):
-            return (np.reshape(np.array(data),(len(data),imgSize[0],imgSize[1],1)), np.array(labels))
-        else:
-            return (np.reshape(np.array(data),(len(data),imgSize[0],imgSize[1],3)), np.array(labels))
-
-
-def prepareArrayImage(array):
-    return prepareImage(PIL.fromarray(array))
-def prepareImage(img):
-    if(isGrayScale):
-        return np.array(img.convert('L').resize(imgSize))
-    else:
-        return np.array(img.convert('RGB').resize(imgSize))
-def prepareFileImage(imgPath):
-    return prepareImage(PIL.open(imgPath))
-
-
-# Debug Setup
-print("Pillow version:", PIL.__version__)
-print("Tensorflow version:", tf.__version__)
-print("Eager execution:", tf.executing_eagerly())
-filePath = path.dirname(path.realpath(__file__))
-print("Application directory:", filePath)
+##Debug Setup
+print('Pillow Version:', PIL.__version__)
+print("Tensorflow is running at: ",tf.__version__)
+print("Eager execution: ",tf.executing_eagerly())
+filePath = (path.dirname(path.realpath(__file__)))
+print("Applicaiton running in "+filePath)
 saveDir = path.dirname(saveLocation)
-print("Save path:", saveDir)
-print(f"Loading from {filePath}\\{saveLocation}")
-print("Exposed GPUs:", tf.config.experimental.list_physical_devices('GPU'))
-print("Exposed CPUs:", tf.config.experimental.list_physical_devices('CPU'), '\n')
-print("Looking for dataset directory: ", end="")
+print("Saving to "+saveDir)
+print("Loading From "+filePath+"\\"+saveLocation)
+print("Exposed GPU's "+str(tf.config.experimental.list_physical_devices('GPU')))
+print("Exposed CPU's "+str(tf.config.experimental.list_physical_devices('CPU')))
+print()
+print("Looking for dataset directory: ",end="")
 
 ##Get files
 directories = [f for f in listdir(filePath) if path.isdir(join(filePath, f))]
@@ -88,17 +62,16 @@ assert datasetName in directories
 for directory in directories:
     print(" - "+str(directory),end="")
     if(str(directory) == datasetName):
-        print("*")
+        print("*")        
     else:
         print()
 print()
 print("Getting Classifiers "+str(datasetName))
-directories = [f for f in listdir(f'{filePath}\\{datasetName}') if path.isdir(join(f'{filePath}\\{datasetName}', f))]
+directories = [f for f in listdir(filePath+"\\"+datasetName) if path.isdir(join(filePath+"\\"+datasetName, f))]
 for directory in directories:
-    print(" - "+str(directory) + " > "+str(len([f for f in listdir(f'{filePath}\\{datasetName}\\{str(directory)}') if path.isfile(join(filePath+"\\"+datasetName+"\\"+str(directory), f))])) + "# files")
+    print(" - "+str(directory) + " > "+str(len([f for f in listdir(filePath+"\\"+datasetName+"\\"+str(directory)) if path.isfile(join(filePath+"\\"+datasetName+"\\"+str(directory), f))])) + "# files")
     
 print()
-
 ##Actual Doing stuff
 print("Turning Data into an array")
 ##Of course not a np array I hate those and won't use them unless I have to, even if they are more effecient. 
@@ -107,11 +80,15 @@ print("Turning Data into an array")
 masterFileArray = []
 for i in range(len(directories)):
     files = [f for f in listdir(filePath+"\\"+datasetName+"\\"+str(directories[i])) if path.isfile(join(filePath+"\\"+datasetName+"\\"+str(directories[i]), f))]
-    for file in files:
-        oneHot = [0] * len(directories)
-        oneHot[i] = 1
-        masterFileArray.append((filePath+"\\"+datasetName+"\\"+str(directories[i])+"\\"+str(file),oneHot))
-
+    if(len(directories) != 2):
+        for file in files:
+            oneHot = [0] * len(directories)
+            oneHot[i] = 1
+            masterFileArray.append((filePath+"\\"+datasetName+"\\"+str(directories[i])+"\\"+str(file),oneHot))
+    else:
+        for file in files:
+            oneHot = i
+            masterFileArray.append((filePath+"\\"+datasetName+"\\"+str(directories[i])+"\\"+str(file),oneHot))
 print("Shuffling.")
 random.seed(randomSeed) #Setting a seed allows re-training the same model without contaminiating the test dataset
 random.shuffle(masterFileArray)
@@ -129,6 +106,50 @@ print("testFiles : "+str(len(testFiles)))
 print("trainFiles : "+str(len(trainFiles)))
 print()
 
+##Functions & Classes
+
+class KerasGeneratorFromFile(keras.utils.Sequence) :
+
+    def __init__(self, fileArray, batch_size) :
+        self.fileArray = fileArray
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return int(ceil(float(len(self.fileArray)) / float(self.batch_size)))
+
+    def __getitem__(self,idx):
+        #assert (((idx+1) * self.batch_size) <= len(self.fileArray))
+        data = []
+        labels = []
+        for fileID in range((idx * self.batch_size),((idx+1) * self.batch_size)-1):
+            if(fileID >= len(self.fileArray)):
+                break
+            data.append(prepareFileImage(self.fileArray[fileID][0]))
+            labels.append(self.fileArray[fileID][1]) ##This is run every time a batch is requested, it is run a bunch and it isn't the fastest. Too Bad!
+        if(len(directories) == 2):
+            trueY = np.array(labels)
+        else:
+            trueY = np.array(labels)
+        
+        if(isGrayScale):
+            trueX = np.reshape(np.array(data),(len(data),imgSize[0],imgSize[1],1))
+        else:
+            trueX = np.reshape(np.array(data),(len(data),imgSize[0],imgSize[1],3))
+        return (trueX,trueY)
+        
+    
+def prepareArrayImage(array):
+    return prepareImage(PIL.fromarray(array))
+def prepareImage(img):
+    if(isGrayScale):
+        return np.array(img.convert('L').resize(imgSize))
+    else:
+        return np.array(img.convert('RGB').resize(imgSize))
+
+def prepareFileImage(imgPath):
+    return prepareImage(PIL.open(imgPath))
+
+
 #Creating Custom Generators
 print("Creating Generators")
 trainGen = KerasGeneratorFromFile(trainFiles,batchSize)
@@ -136,11 +157,12 @@ testGen = KerasGeneratorFromFile(testFiles,batchSize)
 print("Testing Generator : ",end="")
 sampleImages = trainGen.__getitem__(int(len(trainFiles) // batchSize))
 print("("+str(sampleImages[0].shape)+","+str(sampleImages[1].shape)+") shape returned")
+print(sampleImages[0][0])
+print(sampleImages[1][0])
 
 
 
-
-
+    
 print("Generating Model")
 model = Sequential()
 
@@ -180,10 +202,16 @@ for layer in internalLayers:#Hidden Layers
     model.add(Dropout(dropoutAmount))
 
 #Output Layer
-model.add(Dense(len(directories), activation = "softmax")) #Classification layer or output layer
+if(len(directories) != 2):
+    model.add(Dense(len(directories), activation = "softmax")) #Classification layer or output layer
+    model.compile(optimizer="adam", loss='categorical_crossentropy', metrics=['accuracy'])#Finish the model
 
-#Finish the model
-model.compile(optimizer="adam", loss='categorical_crossentropy', metrics=['accuracy'])
+else:
+    model.add(Dense(1, activation = "sigmoid")) #Classification layer or output layer
+    model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy'])#Finish the model
+
+
+
 
 #Save initial
 #model.save_weights(filePath+"\\"+saveLocation.format(epoch=0))
@@ -230,7 +258,7 @@ if(exportWhenComplete):
 
 print("Starting Demo")
 ##This video camera demo tends to not work as your camera input isn't like the training data at all. It would be fun if it worked but it doesn't
-'''
+
 capture = cv.VideoCapture(camNum)
 while True:
     isTrue,frame = capture.read()
@@ -256,7 +284,6 @@ while True:
         break
 '''
 ##This alternative demo usesing test files. Ensure your trained model is on the same shuffle seed to prevent contamination
-print(int(len(testFiles)/batchSize))
 for batchNum in range(int(len(testFiles)/batchSize)):
     batchImg,batchLabel = testGen.__getitem__(batchNum)
     predictions = model.predict(batchImg)
@@ -273,3 +300,5 @@ for batchNum in range(int(len(testFiles)/batchSize)):
         cv.waitKey(0)##Wait indefinitly until a key is pressed5
     print(" -- batchNum: "+str(batchNum))
 print("All Done")
+
+'''
